1d297b2f9f351df3f43f2673347df4a9c2ee3da6
ash2kkid <ashwathpremchandh@gmail.com>
2026-04-20 12:27:42 +0530

    Updated Firebase Configuration

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,10 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 from app.core.config import settings
+import os
+import json
 
 # Prevent multiple initializations
 if not firebase_admin._apps:
-    cred = credentials.Certificate(settings.FIREBASE_KEY_PATH)
+    
+    if os.getenv("FIREBASE_KEY"):
+        # ✅ Production (Render)
+        firebase_key = json.loads(os.environ["FIREBASE_KEY"])
+        cred = credentials.Certificate(firebase_key)
+    else:
+        # ✅ Local development
+        cred = credentials.Certificate(settings.FIREBASE_KEY_PATH)
+
     firebase_admin.initialize_app(cred)
 
 # Firestore client
